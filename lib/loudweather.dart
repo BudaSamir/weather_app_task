d86cd749ca5c59164681f342a4cc026a29704216
Buda Samir <Abdulrahman.samir.attia.elseed.96@gmail.com>
2023-08-15 12:00:41 +0300

    Preparing The Project

---
+++ lib/loudweather.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import 'View/Screens/home_screen.dart';
+
+class LoudWeather extends StatelessWidget {
+  const LoudWeather({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Loud Weather',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
